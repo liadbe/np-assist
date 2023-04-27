08b5e3bfb4217da23ce588fda1955f5236bb99a8
liadbel <liad.bela@genesys.com>
2023-04-27 14:20:19 +0300

    add images jpg

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,24 +5,24 @@ import {
   ChevronRightIcon
 } from "@heroicons/react/24/outline";
 
-import detail1 from '../assets/detail1.png';
-import detail2 from '../assets/detail2.png';
-import detail3 from '../assets/detail3.png';
-import detail4 from '../assets/detail4.png';
-import detail5 from '../assets/detail5.png';
-import detail6 from '../assets/detail6.png';
-import detail7 from '../assets/detail7.png';
-import detail8 from '../assets/detail8.png';
-import detail9 from '../assets/detail9.png';
-import detail10 from '../assets/detail10.png';
-import detail11 from '../assets/detail11.png';
-import detail12 from '../assets/detail12.png';
-import detail13 from '../assets/detail13.png';
-import detail14 from '../assets/detail14.png';
-import detail15 from '../assets/detail15.png';
-import detail16 from '../assets/detail16.png';
-import detail17 from '../assets/detail17.png';
-import detail18 from '../assets/detail18.png';
+import detail1 from '../assets/detail1.jpeg';
+import detail2 from '../assets/detail2.jpeg';
+import detail3 from '../assets/detail3.jpeg';
+import detail4 from '../assets/detail4.jpeg';
+import detail5 from '../assets/detail5.jpeg';
+import detail6 from '../assets/detail6.jpeg';
+import detail7 from '../assets/detail7.jpeg';
+import detail8 from '../assets/detail8.jpeg';
+import detail9 from '../assets/detail9.jpeg';
+import detail10 from '../assets/detail10.jpeg';
+import detail11 from '../assets/detail11.jpeg';
+import detail12 from '../assets/detail12.jpeg';
+import detail13 from '../assets/detail13.jpeg';
+import detail14 from '../assets/detail14.jpeg';
+import detail15 from '../assets/detail15.jpeg';
+import detail16 from '../assets/detail16.jpeg';
+import detail17 from '../assets/detail17.jpeg';
+import detail18 from '../assets/detail18.jpeg';
 
 import { IconButton } from "@material-tailwind/react";
 
